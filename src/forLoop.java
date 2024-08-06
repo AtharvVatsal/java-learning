33ffb1101b5dc9107eaa8a3dc2fca4f8705c9544
Atharv Vatsal <atharvvatsal@outlook.com>
2024-08-06 18:32:55 +0530

    .

--- src/forLoop.java
+++ src/forLoop.java
@@ -4,12 +4,13 @@ import java.util.Scanner;
 public class forLoop {
 
     public static void main(String[] args) {
-        //for loop is used to exiecute a block repeated number of times
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter last number: ");
-        int n = in.nextInt();
-        for (int i = 0; i < n; i++) {
-            System.out.println(i + 1);
+        try (//for loop is used to exiecute a block repeated number of times
+        Scanner in = new Scanner(System.in)) {
+            System.out.println("Enter last number: ");
+            int n = in.nextInt();
+            for (int i = 0; i < n; i++) {
+                System.out.println(i + 1);
+            }
         }
 
     }
